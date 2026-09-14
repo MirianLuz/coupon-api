620fd626db4f40ab839c7ffa2c5309981031d164
MirianLuz <61255081+MirianLuz@users.noreply.github.com>
2026-09-13 23:44:02 -0300

    log

--- src/main/java/com/desafio/coupon_api/application/service/CouponServiceImpl.java
+++ src/main/java/com/desafio/coupon_api/application/service/CouponServiceImpl.java
@@ -18,7 +18,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.UUID;
 
 @Service
-@Slf4j
 public class CouponServiceImpl implements CouponUseCases {
 
     private static final Logger log =
